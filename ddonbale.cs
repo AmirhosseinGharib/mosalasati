3b496009ef8f6a96027d816e2f21ef8af20ac380
AmirhosseinGharib <149072424+AmirhosseinGharib@users.noreply.github.com>
2023-11-01 14:13:41 +0330

    Add files via upload

---
+++ ddonbale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace donbale_mosalasaty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lotfan adad ra benevisid ta mohasebeh konam");
+            string ADAD=Console.ReadLine();
+            int n=Convert.ToInt32(ADAD);
+            int donbaleh = (n*(n + 1)) / 2;
+            Console.WriteLine(donbaleh);
+        }
+    }
+}
